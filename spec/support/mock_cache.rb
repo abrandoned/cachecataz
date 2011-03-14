960f65943f33e05e119098073a34c7e5bc6fba9a
Brandon Dewitt <brandon@myjibe.com>
2011-03-14 13:12:20 -0400

    Version bump to 0.1.0

---
+++ spec/support/mock_cache.rb
@@ -0,0 +1,25 @@
+class MockCache
+  def initialize
+    @cache = {}
+  end
+  
+  def read(key)
+    @cache[key].to_s
+  end
+  
+  def write(key, val)
+    @cache[key] = val.to_s
+  end
+  
+  def increment(key)
+    @cache[key] = @cache[key].to_i + 1
+  end
+  
+  def exist?(key)
+    !!@cache[key]
+  end
+  
+  def clear
+    @cache = {}
+  end
+end  
---
+++ spec/support/mock_model.rb
@@ -0,0 +1,29 @@
+class MockModel
+  include Cachecataz
+  
+  cache_scope :test, :id
+  cache_scope :user, :user_id
+  cache_scope :multi, [:id, :user_id]
+
+  def initialize
+    @id = "1"
+    @user_id = "2"
+    @assoc_id = "3"
+  end
+  
+  def id
+    @id
+  end
+  
+  def user_id
+    @user_id
+  end
+  
+  def assoc_id
+    @assoc_id
+  end
+  
+  def attributes
+    {:user_id => @user_id, :id => @id, "assoc_id" => @assoc_id, "extra" => "attributes", "not" => "important", :but => :present}
+  end
+end
